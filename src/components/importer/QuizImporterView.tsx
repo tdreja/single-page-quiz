9a71c8ac975fa0a91fb732f9b48458e139fbaa3e
Thomas Dreja <thomas.dreja@gmail.com>
2025-05-03 15:50:18 +0200

    feat(Upload): Use promise instead of callback

--- src/components/importer/QuizImporterView.tsx
+++ src/components/importer/QuizImporterView.tsx
@@ -1,32 +1,40 @@
 import React, { ReactElement, useCallback, useState } from 'react';
+import { JsonStaticGameData } from '../../model/game/json/game';
+
+async function readJson(input: HTMLInputElement): Promise<JsonStaticGameData | null> {
+    const files = input.files;
+    if (!files || files.length < 1) {
+        return Promise.resolve(null);
+    }
+    return new Promise((resolve) => {
+        const fileReader = new FileReader();
+        fileReader.onload = (data) => {
+            if (!data.target) {
+                resolve(null);
+                return;
+            }
+            const raw = data.target.result;
+            if (!raw) {
+                resolve(null);
+                return;
+            }
+            resolve(JSON.parse(raw as string) as JsonStaticGameData);
+        };
+        fileReader.readAsText(files[0]);
+    });
+}
 
 export const QuizImporterView = (): ReactElement => {
-    const [json, setJson] = useState<string>('');
+    const [json, setJson] = useState<JsonStaticGameData | null>(null);
 
-    const handleFile = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-        const files = event.target.files;
-        console.log('Files', files);
-
-        if (!files || files.length < 1) {
-            return;
-        }
-
-        const reader = new FileReader();
-        reader.onload = (load) => {
-            if (load.target) {
-                const data = load.target.result;
-                if (data) {
-                    setJson(data as string);
-                }
-            }
-        };
-        reader.readAsText(files[0]);
+    const uploadFile = useCallback(async (event: React.ChangeEvent<HTMLInputElement>) => {
+        setJson(await readJson(event.target));
     }, [json]);
 
     return (
         <div>
-            <input type="file" accept="application/json" onChange={handleFile} />
-            <span>{json}</span>
+            <input type="file" accept="application/json" onChange={uploadFile} />
+            <span>{json ? JSON.stringify(json) : 'none'}</span>
         </div>
     );
 };
