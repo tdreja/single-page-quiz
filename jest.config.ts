7da5f3cf6e613ce81d6525add285242d7cfc1700
Thomas Dreja <thomas.dreja@gmail.com>
2025-05-10 10:04:17 +0200

    feat(icon): Integrate icon directly into HTML

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,12 @@
-import type {JestConfigWithTsJest} from 'ts-jest';
+import type { JestConfigWithTsJest } from 'ts-jest';
 
 const config: JestConfigWithTsJest = {
-  verbose: true,
-  testEnvironment: "node",
-  testRegex:  "(/__tests__/.*|(\\.|/)(test|spec))\\.[t]sx?$",
-  transform: {
-    "^.+\.tsx?$": ["ts-jest",{ diagnostics: { ignoreCodes: ['TS151001'] } }]
-  }
+    verbose: true,
+    testEnvironment: 'node',
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[t]sx?$',
+    transform: {
+        '^.+\.tsx?$': ['ts-jest', { diagnostics: { ignoreCodes: ['TS151001'] } }],
+    },
 };
 
 export default config;
