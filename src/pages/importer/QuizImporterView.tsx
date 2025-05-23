82bb21561022590c4907e2816dd2253ce11d3622
Thomas Dreja <thomas.dreja@gmail.com>
2025-05-23 21:40:51 +0200

    feat(YAML): Allow import for all YAMLs

--- src/pages/importer/QuizImporterView.tsx
+++ src/pages/importer/QuizImporterView.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useCallback, useContext, useState } from 'react';
+import React, { ReactElement, useCallback, useContext, useEffect, useState } from 'react';
 import { generateJsonQuestionTable, JsonStaticGameData } from '../../model/game/json/game';
 import { parse } from 'yaml';
 import { QuizTableProps, QuizTableView } from '../QuizTable';
@@ -8,60 +8,34 @@ import { SharedProps } from '../quiz/SharedProps';
 import { I18N } from '../../i18n/I18N';
 import { GameEventContext } from '../../components/common/GameContext';
 import { ExportQuizView } from './ExportQuizView';
-
-async function readYaml(fileList: FileList | null): Promise<JsonStaticGameData | null> {
-    if (!fileList || fileList.length < 1) {
-        return Promise.resolve(null);
-    }
-    return new Promise((resolve) => {
-        const fileReader = new FileReader();
-        fileReader.onload = (data) => {
-            if (!data.target) {
-                resolve(null);
-                return;
-            }
-            const raw = data.target.result;
-            if (!raw) {
-                resolve(null);
-                return;
-            }
-            resolve(parse(raw as string) as JsonStaticGameData);
-        };
-        fileReader.readAsText(fileList[0]);
-    });
-}
+import { ImportedData, importYaml } from './YamlReader';
 
 export const QuizImporterView = ({ shared }: SharedProps): ReactElement => {
     const i18n = useContext(I18N);
-    const [json, setJson] = useState<JsonStaticGameData | null>(null);
+    const [imported, setImported] = useState<ImportedData>({});
     const [previewTable, setPreviewTable] = useState<QuizTable<JsonStaticQuestionData>>(generateJsonQuestionTable({}));
-    const onGameEvent = useContext(GameEventContext);
 
     const uploadFile = useCallback(async (event: React.ChangeEvent<HTMLInputElement>) => {
-        const file = await readYaml(event.target.files);
-        setJson(file);
-        if (file) {
-            setPreviewTable(generateJsonQuestionTable(file));
-        } else {
-            setPreviewTable(generateJsonQuestionTable({}));
-        }
-    }, [json, previewTable]);
+        setImported(await importYaml(event.target.files));
+    }, [imported]);
 
     const dropFile = useCallback(async (event: React.DragEvent<HTMLInputElement>) => {
         event.preventDefault();
-        const file = await readYaml(event.dataTransfer.files);
-        setJson(file);
-        if (file) {
-            setPreviewTable(generateJsonQuestionTable(file));
-        } else {
-            setPreviewTable(generateJsonQuestionTable({}));
-        }
-    }, [json, previewTable]);
+        setImported(await importYaml(event.dataTransfer.files));
+    }, [imported]);
 
     const tableProps: QuizTableProps<JsonStaticQuestionData> = {
         table: previewTable,
         showDetails: !shared,
     };
+
+    useEffect(() => {
+        if (imported.quiz) {
+            setPreviewTable(generateJsonQuestionTable(imported.quiz));
+        } else {
+            setPreviewTable(generateJsonQuestionTable({}));
+        }
+    }, [imported]);
 
     return (
         <div className="d-flex flex-row flex-wrap align-items-baseline gap-4">
@@ -81,8 +55,8 @@ export const QuizImporterView = ({ shared }: SharedProps): ReactElement => {
             </div>
             <div className="card w-100 flex-grow-1">
                 <div className="card-header">{i18n.importer.previewHeader}</div>
-                { !json && (<span className="card-body">{i18n.importer.noNewQuizUploaded}</span>)}
-                { json && (
+                { !imported.quiz && (<span className="card-body">{i18n.importer.noNewQuizUploaded}</span>)}
+                { imported.quiz && (
                     <>
                         <div className="card-body">
                             <QuizTableView {...tableProps} className="card-body" />
